6aa38bd5389b3bf6a31d73fb712543f3f7c804c7
arteboris <arteboris@ukr.net>
2019-10-18 13:41:14 +0300

    Updates

---
+++ precache-manifest.442e1f86e7ed3b1eb17c8e7f426b50a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d4d75c19ade53e5c832532e4a6d243",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "5627c055044632b9d299",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.0f81be12.chunk.css"
+  },
+  {
+    "revision": "10f0a157ccc80bc189cd",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.840e9ab1.chunk.js"
+  },
+  {
+    "revision": "5627c055044632b9d299",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.069b8015.chunk.js"
+  },
+  {
+    "revision": "911313b77b6cc8e3fc26",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime-main.8874a67c.js"
+  }
+]);
